13e53a737a0b0cdb80521609a152b6a4c9dd6bb6
usborn116 <64931297+usborn116@users.noreply.github.com>
2023-03-07 14:20:12 -0800

    Update Graph.js

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -2,23 +2,16 @@ import React from 'react';
 import Plot from 'react-plotly.js';
 
 const Graph=({accounts})=>{
-    const vals = []
-    const labels = []
-    accounts.map((acct) => 
-        vals.push(acct.amount)
-        )
-
-    accounts.map((acct) => 
-        labels.push(acct.name)
-        )
+    const values = accounts.map((account) => account.amount)
+    const labels = accounts.map((account) => account.name)
     
     return (
         <div>
-            <h3 className="chart-title">Total In Accounts: ${vals.reduce((p, a) => p + a, 0)}</h3>
+            <h3 className="chart-title">Total In Accounts: ${values.reduce((p, a) => p + a, 0)}</h3>
     <Plot
         data={
         [{
-            values: vals,
+            values: values,
             labels: labels,
             name: 'Total in Accounts',
             hole: 0.3,
